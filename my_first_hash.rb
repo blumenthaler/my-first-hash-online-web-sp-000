13e888e7b7b680142f5f4488d291de304d50bb1b
Alex Blumenthaler <blumenthaleralex@gmail.com>
2020-06-23 17:30:23 +0000

    Automatically backed up by Learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -1,6 +1,7 @@
 def my_hash
   # use the literal constructor to set the variable, my_hash, equal to a hash with key/value pairs of your choice.
-  
+  things_I_like = {"Island" => "A good novel by Aldous Huxley", "Truth" => "A delicious IPA from Rheingeist", "Crazy Rhythm" => "A fun jazz standard I am learning to play"
+  }
 
   
 end
